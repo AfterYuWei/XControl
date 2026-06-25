9ab667502f199ae5e6a93933b36561dded9c9566
yuwei <yuwei1228@hotmail.com>
2026-06-25 11:39:39 +0800

    feat(ui): 引入发丝阴影体系替代边框与阴影

--- web/src/components/ui/dialog.tsx
+++ web/src/components/ui/dialog.tsx
@@ -31,7 +31,7 @@ const DialogContent = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "relative z-50 w-full max-w-lg rounded-lg border bg-background p-6 shadow-lg",
+      "relative z-50 w-full max-w-lg rounded-lg bg-background p-6 shadow-modal",
       className
     )}
     {...props}
